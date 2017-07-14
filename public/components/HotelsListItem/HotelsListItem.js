9ed6834926783e2cf713ae88cc1ce217abd008dc
Panagiwtis Georgiadis <panagiwtis.geor@gmail.com>
2017-07-14 22:34:27 +0100

    Moved all the HotelListItem related components into their own folder called HotelsListItem

--- public/components/HotelsListItem/HotelsListItem.js
+++ public/components/HotelsListItem/HotelsListItem.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 
 import HotelsListItemImage from "./HotelsListItemImage";
 import HotelsListItemInfoContainer from "./HotelsListItemInfoContainer";
--- public/components/HotelsListItem/HotelsListItemUserRatingContainer.js
+++ public/components/HotelsListItem/HotelsListItemUserRatingContainer.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 
 import UserRatingRibbonRow from "./UserRatingRibbonRow";
 import UserRatingScore from "./UserRatingScore";
