a3c799e479aab9bcf4ca35b5a479b73f16b4dab7
Panagiwtis Georgiadis <panagiwtis.geor@gmail.com>
2017-07-15 20:34:29 +0100

    Changed the way option elements are being rendered in order to increase readability of the component

--- public/components/Header/FilterByNameContainer.js
+++ public/components/Header/FilterByNameContainer.js
@@ -2,37 +2,16 @@ import React from "react";
 
 const FilterByNameContainer = ({ filterHandler }) => {
 
+	let optionsArray = ["None","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+	let optionsElementArray = optionsArray.map((optionText, iterator) => {
+		return <option key = { iterator } value = { optionText }> { optionText } </option>
+	});
+
 	return (
 		<div className = "filter_by_name_container">
 			<label for = "name_filter">Name: </label>
 			<select className = "form-control" name = "name_filter" onChange = { filterHandler }>
-				<option>None</option>
-				<option>A</option>
-				<option>B</option>
-				<option>C</option>
-				<option>D</option>
-				<option>E</option>
-				<option>F</option>
-				<option>G</option>
-				<option>H</option>
-				<option>I</option>
-				<option>J</option>
-				<option>K</option>
-				<option>L</option>
-				<option>M</option>
-				<option>N</option>
-				<option>O</option>
-				<option>P</option>
-				<option>Q</option>
-				<option>R</option>
-				<option>S</option>
-				<option>T</option>
-				<option>U</option>
-				<option>V</option>
-				<option>W</option>
-				<option>X</option>
-				<option>Y</option>
-				<option>Z</option>
+				{ optionsElementArray }
 			</select>
 		</div>
 	);
